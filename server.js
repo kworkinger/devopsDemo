6bd6487d655819c4515b996c9be45d66445eb656
Kristian Workinger <kristianworkinger@gmail.com>
2021-11-18 12:07:55 -0700

    rollbar

--- server.js
+++ server.js
@@ -2,18 +2,49 @@ const express = require("express")
 const path = require("path")
 const cors = require("cors")
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '2e86007c55f8403fa4f832f53b3fc193',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 const students = ["Kristian"]
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
+    rollbar.info("HTML file served successfully")
 })
 
 app.get('api/students', (req, res) => {
+    rollbar.info("Someone got the list of students on page load")
     res.status(200).send(students)
 })
 
+app.post("/api/studnet", (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName => studentName === name)
+
+    if (index === -1 && name){
+        students.push(name)
+        rollbar.log("Student added successfully", {author: "Kristian", type: "manual entry"})
+        res.status(200).send(students)
+    } else if(!name){
+        rollbar.error("No name given")
+        res.status(400).send("Must provide a name")
+    } else {
+        rollbar.error("Student already exists")
+        res.status(400).send("That student already exists")
+    }
+})
 
 const port = process.env.PORT || 4545
 
